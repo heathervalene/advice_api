ea965258f3706aa93e9970473a8bc8cfb24a957d
HeatherRouke <heather214@gmail.com>
2024-03-14 13:15:04 -0400

    setup react and axios

---
+++ src/components/AdviceDetal.jsx
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+
+
+const AdviceDetail = () => {
+    return (
+        <div>
+            <h1>Advice Detail</h1>
+        </div>
+    )
+}
+
+export default AdviceDetail;
